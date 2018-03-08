f2dc42727756e7480da17b10682f51efc85552fe
Ankeet Maini <ankeet.maini@gmail.com>
2018-03-08 19:36:18 +0530

    Adds a TS class

--- exercises/dealer/src/dealer.ts
+++ exercises/dealer/src/dealer.ts
@@ -12,6 +12,62 @@ function shuffleArray(a: any[]) {
   }
 }
 
-// export class Dealer {
+export enum Suit {
+  Clubs,
+  Diamonds,
+  Hearts,
+  Spades
+}
 
-// }
+export enum CardNumber {
+  Ace,
+  Two,
+  Three,
+  Four,
+  Five,
+  Six,
+  Seven,
+  Eight,
+  Nine,
+  Ten,
+  Jack,
+  Queen,
+  King
+}
+
+type Card = [Suit, CardNumber];
+
+function createDeck(): Card[] {
+  const allSuits = Object.keys(Suit).slice(0, 4);
+  const allCards = Object.keys(CardNumber).slice(0, 13);
+
+  return allSuits.reduce((all: Card[], suit: string) => {
+    return allCards.reduce((acc: Card[], card: string) => {
+      return [...acc, <Card>[Number(suit), Number(card)]];
+    }, all);
+  }, []);
+}
+
+export class Dealer {
+  cards: Card[] = [];
+
+  constructor() {
+    this.cards = createDeck();
+    shuffleArray(this.cards);
+  }
+
+  getLength(): number {
+    return this.cards.length;
+  }
+
+  readCard(card: Card): string {
+    const [suit, cardNumber] = card;
+    return `${CardNumber[cardNumber]} of ${Suit[suit]}`;
+  }
+
+  dealHand(numCards: number): Card[] {
+    if (numCards > this.cards.length) throw new Error('Not enough cards');
+    if (numCards < 0) throw new Error('Cannot give negative cards');
+    return this.cards.splice(0, numCards);
+  }
+}
