1b2d580d16d52a7e147c6cfb7d5bdb82931a20e9
Ankeet Maini <ankeet.maini@gmail.com>
2018-03-10 00:22:21 +0530

    Adds a Stack class implementation in TS

--- exercises/stack/src/stack.ts
+++ exercises/stack/src/stack.ts
@@ -6,6 +6,27 @@ interface IStack<T> {
   print(): void;
 }
 
-// export class Stack<T> {
+export class Stack<T> implements IStack<T> {
+  items: T[] = [];
 
-// }
+  pop(): T {
+    return this.items.splice(-1)[0];
+  }
+
+  push(items: T | T[]): IStack<T> {
+    if (Array.isArray(items)) {
+      this.items = [...this.items, ...items];
+    } else {
+      this.items.push(items);
+    }
+    return this;
+  }
+
+  length(): number {
+    return this.items.length;
+  }
+
+  print() {
+    console.log(this.items.join(','));
+  }
+}
