8fc1fe67a062be258b9bf1ccd6aa86811e7298e2
Ankeet Maini <ankeet.maini@gmail.com>
2018-03-10 00:36:48 +0530

    Adds a generator fibonacci function

--- exercises/fib/src/fib.ts
+++ exercises/fib/src/fib.ts
@@ -1,8 +1,14 @@
-
 /**
  * Implement a generator function that can be used
  * to generate numbers in the Fibonacci Sequence
  */
-// export function* getFibSequence(): IterableIterator<number> {
- 
-// }
+export function* getFibSequence(): IterableIterator<number> {
+  let lastLast = 1;
+  let last = 0;
+  while (true) {
+    let sum = lastLast + last;
+    lastLast = last;
+    yield sum;
+    last = sum;
+  }
+}
